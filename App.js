9c0d63233943cb43315a5eab65ab99fcc794141e
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-03-18 18:51:31 -0300

    primiera versao alcoolxgasosa

--- App.js
+++ App.js
@@ -1,11 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { React, useState } from 'react';
 
 export default function App() {
+  const[valor1, setValor1] = useState();
+  const[valor2, setValor2] = useState();
+  const[resultado, setResultado] = useState();
+  const[resposta, setResposta] = useState();
+
+
+
+
+  function dividir(){
+    setResultado (parseFloat(valor1) / parseFloat(valor2));
+
+    if(resultado < 0.7){
+      setResposta("Abasteça com álcool");
+    }else{
+      setResposta("Abasteça com gasolina");
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.texto}>Álcool, ou gasolina?</Text>
+      <View style={styles.bloco}>
+        <Text style={styles.textoBloco}>Valor do litro do alcool</Text>
+        <TextInput 
+          style={styles.input}
+          keyboardType="numeric"
+          value={valor1}
+          onChangeText={(texto)=>setValor1(texto)}
+        />
+      </View>
+      <View style={styles.bloco}>
+        <Text style={styles.textoBloco}>Valor do litro da gasolina</Text>
+        <TextInput 
+          style={styles.input}
+          keyboardType="numeric"
+          value={valor2}
+          onChangeText={(texto)=>setValor2(texto)}
+        />
+      </View>
+      <View style={styles.bloco}>
+        <TouchableOpacity 
+          style={styles.botao}
+          onPress={dividir}
+          >
+            <Text style={styles.textoBotao}>Dividir</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.bloco}>
+            <Text style={styles.textoBloco}>Resultado: {resultado} - {resposta}</Text>
+      </View>
     </View>
   );
 }
@@ -13,8 +59,36 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFF',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textoBloco:{
+    fontSize:30,
+    color: '#39b1b8',
+  },
+  texto:{
+    color:'#000',
+    fontSize:30
+  },
+  input:{
+    borderColor:'#000',
+    borderWidth:2,
+    fontSize:30,
+    width:'80%'
+  },
+  bloco:{
+    width:'100%',
+    alignItems:'center',
+    marginTop:30
+  },
+  botao:{
+    backgroundColor:'#39b1b8',
+    width:'80%',
+    textAlign:'center'
+  },
+  textoBotao:{
+    color:"#fff",
+    fontSize:30
+  }
 });
